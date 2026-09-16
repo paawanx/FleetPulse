45999f3ca59b9647ad32aebdfea44f704be9bb1c
Paawan Srivastava <136116866+paawanx@users.noreply.github.com>
2026-09-16 17:41:23 -0600

    Add logging for worker

--- FleetPulse.Worker/TelemetryConsumer.cs
+++ FleetPulse.Worker/TelemetryConsumer.cs
@@ -18,20 +18,32 @@ public class TelemetryConsumer : IConsumer<VehicleTelemetryEvent>
 
     public async Task Consume(ConsumeContext<VehicleTelemetryEvent> context)
     {
-        var telemetry = context.Message;
-        _logger.LogInformation("Received telemetry for VehicleId: {VehicleId}, Latitude: {Latitude}, Longitude: {Longitude}, Speed: {Speed}, FuelLevel: {FuelLevel}, Timestamp: {Timestamp}",
-            telemetry.VehicleId, telemetry.Latitude, telemetry.Longitude, telemetry.Speed, telemetry.FuelLevel, telemetry.Timestamp);
+        try{
+            var telemetry = context.Message;
+            _logger.LogInformation("Received telemetry for VehicleId: {VehicleId}, Latitude: {Latitude}, Longitude: {Longitude}, Speed: {Speed}, FuelLevel: {FuelLevel}, Timestamp: {Timestamp}",
+                telemetry.VehicleId, telemetry.Latitude, telemetry.Longitude, telemetry.Speed, telemetry.FuelLevel, telemetry.Timestamp);
 
-        _dbContext.Telemetries.Add(new Telemetry
+            _dbContext.Telemetries.Add(new Telemetry
+            {
+                VehicleId = telemetry.VehicleId,
+                Latitude = telemetry.Latitude,
+                Longitude = telemetry.Longitude,
+                SpeedInKmh = telemetry.Speed,
+                FuelLevelInPercentage = telemetry.FuelLevel,
+                Timestamp = telemetry.Timestamp
+            });
+
+            await _dbContext.SaveChangesAsync();
+
+            _logger.LogInformation("Telemetry data saved to database for VehicleId: {VehicleId}", telemetry.VehicleId);
+        }
+        catch(Exception ex)
         {
-            VehicleId = telemetry.VehicleId,
-            Latitude = telemetry.Latitude,
-            Longitude = telemetry.Longitude,
-            SpeedInKmh = telemetry.Speed,
-            FuelLevelInPercentage = telemetry.FuelLevel,
-            Timestamp = telemetry.Timestamp
-        });
-
-        await _dbContext.SaveChangesAsync();
+            _logger.LogError(ex, "Error processing telemetry message: {Message}", context.Message);
+            throw; // Optionally rethrow to let MassTransit handle the failure (e.g., retry, dead-letter)
+        }finally
+        {
+            _logger.LogInformation("Finished processing telemetry message for VehicleId: {VehicleId}", context.Message.VehicleId);
+        }
     }
 }
