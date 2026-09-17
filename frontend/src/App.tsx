3c3df22bfb9eef55c5b6fed6536851595b5e83ea
Paawan Srivastava <136116866+paawanx@users.noreply.github.com>
2026-09-16 23:59:28 -0600

    added build and push for worker and frontend along with if condition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import VehicleDashboard from './vehicles/VehicleDashboard';
 
 function App() {
-  
+  // This is a small comment to trigger a change in the file and test the GitHub Actions workflow.
+
   return (
     <div>
       <h1>FleetPulse</h1>
